94702120f57b4a18126d25de6886bd848bb2c0fe
hassan ali <hassanali0111272@gmail.com>
2025-03-04 23:22:04 +0500

    class 02 practice done

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Products from './Components/Products/Products';
+// import logo from './logo.svg';
+import "./App.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Products from "./Components/Products/Products";
+import { useState } from "react";
+import Card3 from "./Components/Cards/Card3";
 
 function App() {
+  const [text, setText] = useState("hello world")
   const gSHock = [
     {
       name: "G-Shock Casio 08",
@@ -13,7 +18,7 @@ function App() {
       fullName:
         "Casio Analog-Digital Black Dial Women’s Watch-BA-110RG-1ADR (BX157)",
       category: "Women's Watch",
-      color: [, "golden", "silver"],
+      color: ["golden", "silver"],
       Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
       img: "https://iwc.com.pk/wp-content/uploads/2020/12/BA-110RG-1ADR-1.jpg",
     },
@@ -25,7 +30,7 @@ function App() {
 
       fullName: "G-shock Casio 09 Champagne Index - 126231-62801",
       category: "Men's Watch",
-      color: [, "golden", "silver"],
+      color: ["golden", "silver"],
       Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
       img: "https://iwc.com.pk/wp-content/uploads/2022/01/GM-S2100-3ADR-1.jpg",
     },
@@ -34,7 +39,7 @@ function App() {
       price: "$600",
       fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
       category: "Men's Watch",
-      color: [, "golden", "silver"],
+      color: ["golden", "silver"],
       Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
       img: "https://iwc.com.pk/wp-content/uploads/2023/03/GMA-S2100-1ADR.jpg",
     },
@@ -46,7 +51,7 @@ function App() {
 
       fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
       category: "Men's Watch",
-      color: [, "golden", "silver"],
+      color: ["golden", "silver"],
       Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
       img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2000S-1ADR.jpg",
     },
@@ -58,7 +63,7 @@ function App() {
 
       fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
       category: "Men's Watch",
-      color: [, "golden", "silver"],
+      color: ["golden", "silver"],
       Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
       img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2100-1A3DR.jpg",
     },
@@ -70,7 +75,7 @@ function App() {
 
       fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
       category: "Men's Watch",
-      color: [, "golden", "silver"],
+      color: ["golden", "silver"],
       Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
       img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2100FR-5ADR.jpg",
     },
@@ -82,7 +87,7 @@ function App() {
 
       fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
       category: "Men's Watch",
-      color: [, "golden", "silver"],
+      color: ["golden", "silver"],
       Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
       img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2200SL-8ADR.jpg",
     },
@@ -94,13 +99,14 @@ function App() {
 
       fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
       category: "Men's Watch",
-      color: [, "golden", "silver"],
+      color: ["golden", "silver"],
       Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
       img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-700BP-1ADR.jpg",
     },
   ];
   return (
-    <div className="App">
+    <>
+    <Card3 name={"Hassan Ali"} email={"dev.hassan@gmail.com"}/>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -115,16 +121,72 @@ function App() {
           Learn React
         </a>
       </header> */}
-      
-     { 
-        gSHock.map((product,index) =>{
-          return <Products img={product.img} name={product.name} caption={product.caption}  price={product.price}  />;
-        })
-          
 
+      <h2>{ text }</h2>
+      <button onClick={()=>{setText("Saylani Mass IT Training")}}>Change Text</button>
+
+      {
+        <Row>
+          {gSHock.map((value, index) => {
+            return (
+              <Col key={index} xs={12} sm={12} md={6} lg={4}>
+                <Products
+                  productName={value.name}
+                  productImg={value.img}
+                  price={value.price}
+                />
+              </Col>
+            );
+          })}
+        </Row>
       }
-    </div>
+      {
+        <Row>
+          {gSHock.map((product, index) => {
+            return (
+              <Col key={index} xs={12} sm={12} md={6} lg={4}>
+                <Products
+                  key={index}
+                  img={product.img}
+                  name={product.name}
+                  caption={product.caption}
+                  price={product.price}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      }
+    </>
   );
 }
 
 export default App;
+
+
+// {
+//   gSHock.map((product, index) => {
+//     return (
+//       <Col
+//         key={index}
+//         xs={12}
+//         sm={12}
+//         md={6}
+//         lg={2}
+//         style={
+//           {
+//             // border: "1px solid black",
+//           }
+//         }
+//       >
+//         <Products
+//           key={index}
+//           img={product.img}
+//           name={product.name}
+//           caption={product.caption}
+//           price={product.price}
+//         />
+//       </Col>
+//     );
+//   });
+// }
---
+++ newapp/src/Components/Cards/Card1.jsx
@@ -0,0 +1,12 @@
+const Card1 = ({name,email}) =>{
+
+  return (
+    <>
+    <h2>{name}</h2>
+    <h3>{email}</h3>
+
+    </>
+  )
+}
+
+export default Card1
